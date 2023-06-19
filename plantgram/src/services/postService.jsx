6d0241fd0208300074102aea8e9f76716c123e83
Yash Patidar <yashpatidar964@gmail.com>
2023-06-20 01:14:34 +0530

    Fix : changed the fetch call location to postServices

--- plantgram/src/services/postService.jsx
+++ plantgram/src/services/postService.jsx
@@ -1,2 +1,15 @@
 import axios from "axios";
 
+export  const getPostData = async (dataDispatch) => {
+    try {
+      const {
+        status,
+        data: { posts },
+      } = await axios.get("/api/posts");
+      if (status === 200 || status === 201) {
+        dataDispatch({ type: "Get_All_Post", payload: posts });
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
